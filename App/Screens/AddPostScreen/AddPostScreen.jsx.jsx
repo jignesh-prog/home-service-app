723d27613b37ee302a7bccb50365fa5625bca9a7
Jignesh <115460882+jignesh-prog@users.noreply.github.com>
2024-03-12 20:24:11 +1300

    Image add function

--- App/Screens/AddPostScreen/AddPostScreen.jsx.jsx
+++ App/Screens/AddPostScreen/AddPostScreen.jsx.jsx
@@ -1,33 +1,56 @@
-import { View, Text, TextInput,StyleSheet,Button} from 'react-native'
+import { View, Text, TextInput,StyleSheet,Button,Image, ScrollView, KeyboardAvoidingView, TouchableOpacity} from 'react-native'
 import React from 'react'
 import { app } from '../../../firebaseConfig'
 import {getFirestore,collection, getDocs} from "firebase/firestore"
 import {useEffect,useState} from "react";
 import {Picker} from "@react-native-picker/picker"
 import { Formik } from 'formik';
+import * as ImagePicker from 'expo-image-picker';
 export default function AddPostScreen() {
   const db = getFirestore(app);
   const [categoryList,setCategoryList] = useState([])
+  const [image, setImage] = useState(null);
   useEffect(()=> {
     getCategoryList();
-    
-  },[])
+  },[]);
   const getCategoryList = async() => {
-    const querySnapshot = await getDocs(collection(db, "cities"));
+    setCategoryList([])
+    const querySnapshot = await getDocs(collection(db, 'Category'));
 querySnapshot.forEach((doc) => {
-  // doc.data() is never undefined for query doc snapshots
-  console.log(doc.id, " => ", doc.data());
+  console.log("Docs:", doc.data());
   setCategoryList(categoryList=>[...categoryList,doc.data()])
 });
-
   }
+  {/*Imag picker*/}
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 4],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
   return (
-    <View style={{padding:10}}>
-    <Formik
-    initialValues={{title:"",add:"",category:"",desc:"",price:""}}
+    <KeyboardAvoidingView style={{padding:10,}}>
+        <ScrollView>
+   <Text style={styles.text}>Add New Post</Text>
+      <Formik
+    initialValues={{title:"",desc:"",price:"",add:"",category:""}}
     onSubmit={value=>console.log(value)}> 
-    {({handleChange,handleBlur,handleSubmit,values}) => (
+    {({handleChange,handleSubmit,values}) => (
 <View>
+  <TouchableOpacity onPress={pickImage}>
+    {image? 
+    <Image source={{uri: image}} style={styles.image} />
+    :
+  <Image style={styles.image} source={require('../../../assets/uploadImage.png')}/>}
+  </TouchableOpacity>
   <TextInput 
   style={styles.input} 
   placeholder='Title'
@@ -38,7 +61,7 @@ onChangeText={handleChange("title")}
   style={styles.input} 
   placeholder='Description'
 value={values?.desc}
-numberOfLines={5}
+ numberOfLines={5}
 onChangeText={handleChange("desc")}
   />
   <TextInput 
@@ -48,20 +71,27 @@ value={values?.price}
 keyboardType='number-pad'
 onChangeText={handleChange("price")}
   />
-
-  <Picker
+  <TextInput 
   style={styles.input} 
+  placeholder='Add'
+value={values?.add}
+onChangeText={handleChange("add")}
+  />
+{/*category list dropdown*/}
+  <Picker
   selectedValue={values?.category}
-onChangeText={handleChange("category")}>
-  {categoryList&&categoryList.map((item,index) =>(
-    <Picker.Item key={index} label={item.name} value={item.name} />
+  style={styles.input}
+onValueChange={handleChange("category")}>
+  {categoryList && categoryList.map((item,index) =>(
+    <Picker.Item key={index} label={item?.Name} value={item?.Name} />
   ))}
   </Picker>
   <Button onPress={handleSubmit} title="submit"/>
 </View>
 )}
     </Formik>
-    </View>
+    </ScrollView>
+    </KeyboardAvoidingView>
   )
 }
 const styles =StyleSheet.create({
@@ -71,7 +101,17 @@ const styles =StyleSheet.create({
     padding:10,
     margin:10,
  paddingHorizontal:17,
- fontSize:17
-  
+ fontSize:17,  
+ borderWidth:1.5
   },
+  text:{
+    margin:10,
+    fontSize:20,
+    fontWeight:"bold"
+  },
+  image:{
+    width:100,
+    height:100,
+    borderRadius:15
+  }
 })
